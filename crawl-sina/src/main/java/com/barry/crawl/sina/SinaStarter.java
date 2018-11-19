5e62dac2d70caa689909f5827f2b30756155e4e2
hts <htbyanghang2@163.com>
2018-11-19 23:13:10 +0800

    init commit

---
+++ crawl-sina/src/main/java/com/barry/crawl/sina/SinaStarter.java
@@ -0,0 +1,24 @@
+package com.barry.crawl.sina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+/**
+ * TaskApplication [spring boot] 主方法
+ *
+ * 启动方式，右键->run/debug->Spring Boot App
+ *
+ * @author
+ *
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@ServletComponentScan
+public class SinaStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinaStarter.class, args);
+    }
+
+}
