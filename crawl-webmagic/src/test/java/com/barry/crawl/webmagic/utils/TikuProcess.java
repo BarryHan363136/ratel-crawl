9bf9a1eb099e430fdd175864f6f036e87576d263
Tongshan Han <Tongshan.Han@partner.bmw.com>
2018-11-29 18:07:08 +0800

    init commit

---
+++ crawl-webmagic/src/test/java/com/barry/crawl/webmagic/utils/TikuProcess.java
@@ -0,0 +1,22 @@
+package com.barry.crawl.webmagic.utils;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+/**
+ * https://blog.csdn.net/chen45682kang/article/details/75810033
+ * */
+public class TikuProcess implements PageProcessor {
+
+    @Override
+    public void process(Page page) {
+
+    }
+
+    @Override
+    public Site getSite() {
+        return null;
+    }
+
+}
